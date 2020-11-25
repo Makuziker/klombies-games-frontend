6c10224d7a6cdcedf28c289db9ff66426571a6cd
Brogan <makuziker@protonmail.com>
2020-11-25 09:54:24 -0700

    upgraded to socket.io-client v3, firebase deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 
 import { ROUTES } from './constants';
@@ -19,5 +19,5 @@ export default function App() {
         </Switch>
       </Layout>
     </Providers>
-  )
+  );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,13 @@
-import socketIO from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 export type IRouteKey = 'home' | 'room' | 'game';
 
 export const ROUTES: Record<IRouteKey, string> = {
   home: '/',
   room: '/room/:id',
-  game: '/game/:id'
+  game: '/game/:id',
 };
 
-export const API_ENDPOINT = 'localhost:8080';
-export const socket = socketIO(API_ENDPOINT);
+export const API_ENDPOINT = 'http://localhost:4000';
+
+export const socket = io(API_ENDPOINT);
